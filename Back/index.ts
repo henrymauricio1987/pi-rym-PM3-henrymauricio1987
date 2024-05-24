370ff422fe74a62a6b68e1d3c83b2d005ed5b2a0
henrymauricio1987 <hmauricio1987@gmail.com>
2024-05-24 11:02:33 -0500

    HW 1

---
+++ Back/index.ts
@@ -0,0 +1,40 @@
+// const num1 = 12;
+// const num2 = 20;
+// const lastnames: string = "osorio";
+
+// const sumar = (a: number, b:number):number => (a + b);
+
+// console.log(sumar(num1, num2));
+
+
+// console.log(lastnames);
+
+// const names: string[] = [" a ", " b ", " c "]
+
+// const arg1: string = "hola";
+
+// console.log(arg1)
+
+// const num1: number = 23;
+// const num2: number = 10;
+
+// const sumar = (suma: number) => {
+//     return `El total de la suma es: ${suma}`
+// }
+
+// console.log(sumar(num1 + num2));
+
+type TbloqueGeneral {
+    lado: number;
+    alto: number;
+    color: string;
+    tamaño: number;
+}
+
+const cubo: TbloqueGeneral = {
+    lado: 22,
+    alto: 10,
+    color: "amarillo",
+    tamaño: 100,
+}
+
