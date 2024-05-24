97808aabb4f5b080b6afdb0e7ee84362d6be9f0c
henrymauricio1987 <hmauricio1987@gmail.com>
2024-05-24 17:22:15 -0500

    Expreess y Typescript

---
+++ back/src/index.ts
@@ -0,0 +1,11 @@
+const express = require("express");
+// require("dotenv").config()
+
+// const PORT = process.env.PORT || 3000
+
+const server = express();
+
+server.listen(3000, () => {
+    console.log("Server listening on PORT 3000");
+})
+
